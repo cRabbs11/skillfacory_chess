3ce680506e7e63bfdac5eacca5c4f2951775614c
evgenykochkov <crabbs100@gmail.com>
2021-07-13 21:15:34 +0300

    доделал классы ChessBoard и ChessPiece

--- src/ChessPiece.java
+++ src/ChessPiece.java
@@ -1,7 +1,8 @@
 abstract public class ChessPiece {
 
     protected String color;
-    private boolean check = true;
+
+    public boolean check = true;
 
     public ChessPiece(String color) {
         this.color = color;
@@ -12,5 +13,27 @@ abstract public class ChessPiece {
     abstract boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn);
 
     abstract String getSymbol();
+
+    abstract protected boolean isCollisionOnMove(ChessBoard chessBoard, int line, int column, int toLine, int toColumn);
+
+    boolean isMoveOnSamePosition(int line, int column, int toLine, int toColumn) {
+        return (line==toLine && column==toColumn);
+    }
+
+    boolean isMoveOffTheBoard(int line, int column, int toLine, int toColumn) {
+        return (toLine<0 || toLine>7 || toColumn<0 || toColumn>7);
+    }
+
+    protected boolean isCanEating(ChessBoard chessBoard, int toLine, int toColumn) {
+        if (chessBoard.board[toLine][toColumn]==null) {
+            return true;
+        } else {
+            if (!chessBoard.board[toLine][toColumn].color.equals(chessBoard.nowPlayer)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
 
 }
